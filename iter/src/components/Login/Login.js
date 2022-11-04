d37c4125535ecffceb93d43b7d9e29cde4f6b37e
HJE <hanzziing@gmail.com>
2022-11-04 12:07:32 +0900

    je pushhh2

--- iter/src/components/Login/Login.js
+++ iter/src/components/Login/Login.js
@@ -1,35 +1,28 @@
 import { useRef, useState } from "react";
-import * as React from 'react';
+import * as React from "react";
 import axios from "axios";
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 
 import image from "./images/main.jpg";
 import logo from "./images/mainlogo.png";
 import "./login.scss";
 
-
-
-
-
 function Copyright(props) {
-  return (
-   <></>
-  );
+  return <></>;
 }
 
 const theme = createTheme();
 
 export default function Login() {
-
   const idRef = useRef();
   const pwRef = useRef();
   const nav = useNavigate();
@@ -39,23 +32,23 @@ export default function Login() {
     console.log("handleLogin!");
     console.log(idRef.current.value);
     console.log(pwRef.current.value);
-  
+
     // axios.post("보낼 위치", "보낼 데이터")
-    axios.post("http://127.0.0.1:3001/Login", {
+    axios
+      .post("http://127.0.0.1:3001/Login", {
         email: idRef.current.value,
         password: pwRef.current.value,
-    })
-    .then((result)=>{
-        console.log("데이터 보내기 성공!", result.data.id)
+      })
+      .then((result) => {
+        console.log("데이터 보내기 성공!", result.data.id);
+        localStorage.setItem("id", idRef.current.value);
         nav("/MainView");
-     
-    }) // axios로 보낼 위치에 데이터 보내기를 성공하면 then
-    .catch(()=>{
-        console.log("데이터 보내기 실패!")
-    }); // aixos로 보낼 위치에 데이터 보내기를 실패하면 catch
-  
+      }) // axios로 보낼 위치에 데이터 보내기를 성공하면 then
+      .catch(() => {
+        console.log("데이터 보내기 실패!");
+      }); // aixos로 보낼 위치에 데이터 보내기를 실패하면 catch
   };
-  
+
   // const handleSubmit = (event) => {
   //   event.preventDefault();
   //   const data = new FormData(event.currentTarget);
@@ -66,9 +59,13 @@ export default function Login() {
   // };
 
   return (
-    
     <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '90vh' , width: '140vh' }} className='main'>
+      <Grid
+        container
+        component="main"
+        sx={{ height: "90vh", width: "140vh" }}
+        className="main"
+      >
         <CssBaseline />
         <Grid
           item
@@ -76,87 +73,96 @@ export default function Login() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: `url(${image})`,  // 좌측 이미지
-            backgroundRepeat: 'no-repeat',
+            backgroundImage: `url(${image})`, // 좌측 이미지
+            backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box className='centerbox'
+          <Box
+            className="centerbox"
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
             {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.light' }}> */}
-            <img src={logo} className='logo'></img>
-              {/* <LockOutlinedIcon />  */}
-              {/* 회원가입 위 아이콘 수정자리 */}
+            <img src={logo} className="logo"></img>
+            {/* <LockOutlinedIcon />  */}
+            {/* 회원가입 위 아이콘 수정자리 */}
             {/* </Avatar> */}
             <Typography component="h1" variant="h5">
               로그인
             </Typography>
             {/* <form onSubmit={handleLogin} method="post"> */}
-            <Box component="form" noValidate onSubmit={handleLogin} sx={{ mt: 1 }} method="post">
-            <Box>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="이메일 주소"
-                name="email"
-                autoComplete="email"
-                inputRef={idRef}
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="비밀번호"
-                type="password"
-                id="password"
-                inputRef={pwRef}
-                autoComplete="current-password"
-              />
-              {/* <FormControlLabel
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleLogin}
+              sx={{ mt: 1 }}
+              method="post"
+            >
+              <Box>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="이메일 주소"
+                  name="email"
+                  autoComplete="email"
+                  inputRef={idRef}
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="비밀번호"
+                  type="password"
+                  id="password"
+                  inputRef={pwRef}
+                  autoComplete="current-password"
+                />
+                {/* <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Remember me"
               /> */}
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color='info'
-                sx={{ mt: 3, mb: 2 }}
-              >
-                로그인
-              </Button> 
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    비밀번호 찾기
-                  </Link>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="info"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  로그인
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      비밀번호 찾기
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="/SignIn" variant="body2">
+                      {"아이디가 없나요? 회원가입하기"}
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Link href="/SignIn" variant="body2">
-                    {"아이디가 없나요? 회원가입하기"}
-                  </Link>
-                </Grid>
-              </Grid>
-              <Copyright sx={{ mt: 5 }} />
+                <Copyright sx={{ mt: 5 }} />
+              </Box>
+              {/* </form> */}
             </Box>
-            {/* </form> */}
           </Box>
-            </Box>
         </Grid>
       </Grid>
     </ThemeProvider>
--- router/router.js
+++ router/router.js
@@ -1,24 +1,23 @@
-
 const express = require("express");
 const router = express.Router();
 const mysql = require("mysql2");
 const path = require("path");
-
 
 let conn = mysql.createConnection({
   host: "project-db-stu.ddns.net",
   user: "campus_h_1024_1",
   password: "smhrd1",
   port: "3307",
-  database: "campus_h_1024_1"
+  database: "campus_h_1024_1",
 });
 
-
 let msgarr = [];
+let idqwe;
+let seqwe;
 
-router.get('*',(request,respond)=>{
-      console.log("접속을 환영합니다 FnD 팀!!!")
-      respond.sendFile(path.join(__dirname, "..", "iter", "build", "index.html"));
+router.get("*", (request, respond) => {
+  console.log("접속을 환영합니다 FnD 팀!!!");
+  respond.sendFile(path.join(__dirname, "..", "iter", "build", "index.html"));
 });
 
 router.post("/SignIn", function (request, response) {
@@ -34,54 +33,80 @@ router.post("/SignIn", function (request, response) {
   console.log("사용자가 입력한 성별 : " + request.body.gender);
   console.log("사용자가 입력한 주민번호 : " + userRegNumber);
 
-  let sql = "insert into t_user(user_id, user_pw, user_name, user_joindate, user_type, user_gender, user_regnumber) values(?, ?, ?, sysdate(), '0', ?, ?)";
-  conn.query(sql, [userID, userPW, userName, userGender, userRegNumber], function (err, rows) {
-    if (!err) {
-      console.log("회원가입 완료!");
-      response.json({result:"success Join"})
-    } else {
-      console.log("회원가입 실패!" + err);
+  let sql =
+    "insert into t_user(user_id, user_pw, user_name, user_joindate, user_type, user_gender, user_regnumber) values(?, ?, ?, sysdate(), '0', ?, ?)";
+  conn.query(
+    sql,
+    [userID, userPW, userName, userGender, userRegNumber],
+    function (err, rows) {
+      if (!err) {
+        console.log("회원가입 완료!");
+        response.json({ result: "success Join" });
+      } else {
+        console.log("회원가입 실패!" + err);
+      }
     }
-  });
+  );
 });
 
 router.post("/Login", function (request, response) {
   const userID = request.body.email;
   const userPW = request.body.password;
-  
+
   // response.json({ result: "success" });
-  
+
   let sql = "select * from t_user where user_id=? and user_pw=?";
   conn.query(sql, [userID, userPW], function (err, rows) {
     if (rows.length > 0) {
       request.session.user = {
-        userID: rows[0].id,
-        // userPW: rows[0].pw,
+        userID: rows[0].user_id,
+        userSeq: rows[0].user_seq,
         // userPN: rows[0].phone,
         // userAD: rows[0].address,
-      };      
-      response.json({ result: "success", id: userID});
-      console.log(userID, userPW)
-  
+      };
+      idqwe = request.session.user.userID;
+      seqwe = request.session.user.userSeq;
+
+      response.json({ result: "success", id: userID });
     } else {
-      console.log("로그인 실패")
-    }});
-    
-    // sql = "select * from message where id=?";
-    // conn.query(sql, [userID], function(err, rows){
-  
-    //   if(rows.length>0){
-    //     msgarr=[];          
-    //     for(let i=0; i<rows.length; i++){
-    //       msgarr.push({name : rows[i].name,
-    //       content : rows[i].content});
-    //     };
+      console.log("로그인 실패");
+    }
+  });
 
-    //   } else{
-    //     console.log("메세지가 없음");
-    //   };
-    // });
+  // sql = "select * from message where id=?";
+  // conn.query(sql, [userID], function(err, rows){
 
+  //   if(rows.length>0){
+  //     msgarr=[];
+  //     for(let i=0; i<rows.length; i++){
+  //       msgarr.push({name : rows[i].name,
+  //       content : rows[i].content});
+  //     };
+
+  //   } else{
+  //     console.log("메세지가 없음");
+  //   };
+  // });
+});
+
+router.post("/Userpost", function (request, response) {
+  const post = request.body.post;
+  const location = request.body.location;
+  const id = idqwe;
+  const seq = Number(seqwe);
+
+  console.log(id);
+  let sql =
+    "insert into t_post(user_seq, user_id, post_date, post_location, post_content, post_likes, post_dislikes) values (?, ?, sysdate(), ?, ?, 0, 0)";
+
+  conn.query(sql, [seq, id, location, post, id], function (err, rows) {
+    if (!err) {
+      console.log("업로드 완료!");
+      response.json({ result: "success Join" });
+    } else {
+      console.log("업로드 실패!" + err);
+    }
+  });
 });
 
 module.exports = router;
